1044993c0a7bf742efd00bf74c300b0c4ed5d3cd
Pankaj <bharambeps90@gmail.com>
2023-01-06 20:43:20 +0530

    This is My First Commit

---
+++ src/demo/Test1.java
@@ -0,0 +1,19 @@
+package demo;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		
+		String drink = "Juice";
+		
+		if(drink == "Juice")
+		{
+			System.out.println("Finish");
+		}
+		else
+		{
+			System.out.println("Stay Hungry");
+		}
+	}
+
+}
